b33d1cd3ea2ff96028c73b2b9f57b605d862d805
Duck <coo5555553@gmail.com>
2020-02-11 21:55:22 +0800

    weeb

---
+++ cmds/weeb.py
@@ -0,0 +1,140 @@
+from classCog import Cog_Ext
+from discord.ext import commands
+import random
+import discord
+import json
+import datetime
+import pytz
+
+
+tz = pytz.timezone("Asia/Taipei")
+with open("weeb.json", "r", encoding="utf8") as f:
+    weebs = json.load(f)
+no = discord.Embed(
+    title="You are not My Owner!", 
+    color=discord.Color.dark_red()
+)
+no.set_image(url="https://i.imgur.com/Z67P5RS.gif")
+
+
+class Weeb(Cog_Ext):
+    @commands.command()
+    async def ascii_art(self, ctx, num: int = 0):
+        with open("weeb.json", "r", encoding="utf8") as f:
+            weebs = json.load(f)
+        if str(ctx.author.id) in weebs["log"] and not self.bot.is_owner(ctx.author):
+            Td = weebs["log"][str(ctx.author.id)]
+            delta = Td - datetime.datetime.now(tz).timestamp()
+            if(delta > 0):
+                await ctx.send("{0}，你還要{1}秒才能使用此指令".format(ctx.author.mention, round(delta)))
+                return
+        if num:
+            i = num
+        else:
+            i = random.choice(range(weebs["ascii"]["count"]))
+        await ctx.send(weebs["ascii"][str(i + 1)])
+        weebs["log"][str(ctx.author.id)] = (datetime.datetime.now(tz) + datetime.timedelta(seconds=100)).timestamp()
+        with open("weeb.json", "w", encoding="utf8") as f:
+            json.dump(weebs, f, indent=4, ensure_ascii=False)
+
+
+    @commands.command()
+    async def ascii_list(self, ctx):
+        if not await self.bot.is_owner(ctx.author):
+            await ctx.send(embed=no)
+            return
+        with open("weeb.json", "r", encoding="utf8") as f:
+            weebs = json.load(f)
+        emb = discord.Embed(title="Ascii Art List", color=discord.Colour.gold(), timestamp=datetime.datetime.now(tz))
+        for i in range(weebs["ascii"]["count"]):
+            emb.add_field(name=f"#{i + 1}", value=weebs["ascii"][str(i + 1)], inline=False)
+        await ctx.send(embed=emb)
+
+
+    @commands.command()
+    async def ascii_add(self, ctx, *, msgs):
+        if not await self.bot.is_owner(ctx.author):
+            await ctx.send(embed=no)
+            return
+        try:
+            weebs["ascii"][weebs["ascii"]["count"] + 1] = ""
+            for i in msgs:
+                weebs["ascii"][weebs["ascii"]["count"] + 1] += i
+        except Exception as e:
+            await ctx.send(f"加入失敗，原因為{e}")
+            return
+        weebs["ascii"]["count"] += 1
+        emb = discord.Embed(title="Ascii Art Added", color=discord.Colour.gold(), timestamp=datetime.datetime.now(tz))
+        emb.add_field(name="#{0}".format(weebs["ascii"]["count"]), value=weebs["ascii"][weebs["ascii"]["count"]], inline=False)
+        with open("weeb.json", "w", encoding="utf8") as f:
+            json.dump(weebs, f, indent=4, ensure_ascii=False)
+        await ctx.send(embed=emb)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.content == "AYAYA":
+            with open("weeb.json", "r", encoding="utf8") as f:
+                weebs = json.load(f)
+            if str(msg.author.id) in weebs["log"] and not self.bot.is_owner(msg.author):
+                Td = weebs["log"][str(msg.author.id)]
+                delta = Td - datetime.datetime.now(tz).timestamp()
+                if(delta > 0):
+                    return
+            await msg.channel.send("""
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣬⡛⣿⣿⣿⣯⢻ 
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢻⣿⣿⢟⣻⣿⣿⣿⣿⣿⣿⣮⡻⣿⣿⣧ 
+⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣆⠻⡫⣢⠿⣿⣿⣿⣿⣿⣿⣿⣷⣜⢻⣿ 
+⣿⣿⡏⣿⣿⣨⣝⠿⣿⣿⣿⣿⣿⢕⠸⣛⣩⣥⣄⣩⢝⣛⡿⠿⣿⣿⣆⢝ 
+⣿⣿⢡⣸⣿⣏⣿⣿⣶⣯⣙⠫⢺⣿⣷⡈⣿⣿⣿⣿⡿⠿⢿⣟⣒⣋⣙⠊ 
+⣿⡏⡿⣛⣍⢿⣮⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿ 
+⣿⢱⣾⣿⣿⣿⣝⡮⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣋⣻⣿⣿⣿⣿ 
+⢿⢸⣿⣿⣿⣿⣿⣿⣷⣽⣿⣿⣿⣿⣿⣿⣿⡕⣡⣴⣶⣿⣿⣿⡟⣿⣿⣿ 
+⣦⡸⣿⣿⣿⣿⣿⣿⡛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿ 
+⢛⠷⡹⣿⠋⣉⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⡿⠿⢿⣿⣿⣿⣿⣿⣷⢹⣿⣿ 
+⣷⡝⣿⡞⣿⣿⣿⣿⣿⣿⣿⣿⡟⠋⠁⣠⣤⣤⣦⣽⣿⣿⣿⡿⠋⠘⣿⣿ 
+⣿⣿⡹⣿⡼⣿⣿⣿⣿⣿⣿⣿⣧⡰⣿⣿⣿⣿⣿⣹⡿⠟⠉⡀⠄⠄⢿⣿ 
+⣿⣿⣿⣽⣿⣼⣛⠿⠿⣿⣿⣿⣿⣿⣯⣿⠿⢟⣻⡽⢚⣤⡞⠄⠄⠄⢸⣿
+            """)
+            weebs["log"][str(msg.author.id)] = (datetime.datetime.now(tz) + datetime.timedelta(seconds=100)).timestamp()
+            with open("weeb.json", "w", encoding="utf8") as f:
+                json.dump(weebs, f, indent=4, ensure_ascii=False)
+
+
+
+    @commands.command()
+    async def chika(self, ctx):
+        with open("weeb.json", "r", encoding="utf8") as f:
+            weebs = json.load(f)
+        if str(ctx.author.id) in weebs["log"] and not self.bot.is_owner(ctx.author):
+            Td = weebs["log"][str(ctx.author.id)]
+            delta = Td - datetime.datetime.now(tz).timestamp()
+            if(delta > 0):
+                await ctx.send("{0}，你還要{1}秒才能使用此指令".format(ctx.author.mention, round(delta)))
+                return
+        await ctx.send("""
+⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄ 
+⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ 
+⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ 
+⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ 
+⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ 
+⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ 
+⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ 
+⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ 
+⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ 
+⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ 
+⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ 
+⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ 
+⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ 
+⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃ 
+⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
+        """)
+        weebs["log"][str(ctx.author.id)] = (datetime.datetime.now(tz) + datetime.timedelta(seconds=100)).timestamp()
+        with open("weeb.json", "w", encoding="utf8") as f:
+            json.dump(weebs, f, indent=4, ensure_ascii=False)
+
+
+
+
+def setup(bot):
+    bot.add_cog(Weeb(bot))
